7ff7372236fa9257d8a34adc1feb7b3aea6b8f21
unonao <naokiuno634@gmail.com>
2020-05-16 09:53:37 +0900

    debug and change for konect

--- pyDOS/load_graph.py
+++ pyDOS/load_graph.py
@@ -10,14 +10,19 @@ def load_graph(filepath):
     Load a graph matrix
     Args:
         filepath: filepath. It does not matter whether file extention is 'smat', 'mat' or 'txt'.
+
     """
-    (filepath, ext) = os.path.splitext(filepath)
+    head_tail = os.path.split(filepath)
+    (name, ext) = os.path.splitext(head_tail[-1])
     if ext == '.smat':
         return load_graph_from_smat(filepath)
     elif ext == '.mat':
         return load_graph_from_mat(filepath)
     elif ext == '.txt':
         return load_graph_from_txt(filepath)
+    else:
+        if name == 'out':  # for konect data
+            return load_graph_from_konect(filepath)
 
 
 def load_graph_from_smat(filepath):
@@ -28,7 +33,8 @@ def load_graph_from_smat(filepath):
         mname: matrix name
     """
     # load graph
-    data = np.loadtxt(filepath + '.smat')
+    data = np.loadtxt(filepath)
+    N = data[0, 0]
     data = data[1:, :]
     row = np.append(data[:, 0], data[:, 1])
     col = np.append(data[:, 1], data[:, 0])
@@ -42,8 +48,8 @@ def load_graph_from_smat(filepath):
 
     # load normalized eigenvalues (if exist)
     eig_vals = None
-    if os.path.isfile(filepath + '.smat.normalized.eigs'):
-        eig_vals = np.loadtxt(filepath + '.smat.normalized.eigs')
+    if os.path.isfile(filepath + 'normalized.eigs'):
+        eig_vals = np.loadtxt(filepath + 'normalized.eigs')
 
     # compute eigenvalues (if not exist & possible)
     if eig_vals is None:
@@ -64,8 +70,12 @@ def load_graph_from_mat(filepath, mname='A'):
         filepath: filepath without extension(.mat)
         mname: matrix name
     """
+    (filepath, ext) = os.path.splitext(filepath)
     data = sio.loadmat(filepath)
-    return ss.csr_matrix(data[mname]), data["lambda"].flatten()
+    if "lambda" in data.keys():
+        return ss.csr_matrix(data[mname]), data["lambda"].flatten()
+    else:
+        return ss.csr_matrix(data[mname]), None
 
 
 def load_graph_from_txt(filepath):
@@ -73,9 +83,28 @@ def load_graph_from_txt(filepath):
     Load a graph matrix from adjacency list(txt)
     The ajacency list must be 0-indexed!
     """
-    data = np.loadtxt(filepath + '.txt', comments='#')
+    data = np.loadtxt(filepath, comments='#')
     row = np.append(data[:, 0], data[:, 1])
     col = np.append(data[:, 1], data[:, 0])
+    row = row.astype(int)
+    col = col.astype(int)
+    if data.ndim == 3:  # weighted graph
+        weight = np.append(data[:, 2], data[:, 2])
+    else:  # unweighted graph
+        weight = np.ones(2 * data.shape[0])
+    return ss.csr_matrix((weight, (row, col))), None
+
+
+def load_graph_from_konect(filepath):
+    """
+    Load a graph matrix (konect)
+    The ajacency list must be 1-indexed!
+    """
+    data = np.loadtxt(filepath, comments='%')
+    row = np.append(data[:, 0], data[:, 1])
+    col = np.append(data[:, 1], data[:, 0])
+    row = row - 1
+    col = col - 1
     row = row.astype(int)
     col = col.astype(int)
     if data.ndim == 3:  # weighted graph
